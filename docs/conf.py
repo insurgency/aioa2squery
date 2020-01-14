862b7173aea6303d12719365603e4cb6675ee597
h1nk <12993584+h1nk@users.noreply.github.com>
2020-01-14 11:55:05 -0500

    Update Sphinx config
    The Sphinx config for this project will now pull commonly shared config
    values from the central organization Sphinx docs config. This will help
    keep stuff more DRY and reduce the amount of repeated boilerplate
    between our different projects that are documented with Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -1,53 +1,20 @@
 import os
-import sys
+
+from documentation.conf import *
 
 # Add project root to sys.path for autodoc discoverability
 sys.path.insert(0, os.path.abspath('../'))
 
 project = 'aioa2squery'
-# noinspection PyShadowingBuiltins
-copyright = '2019, insurgency.gg'
-author = 'insurgency.gg'
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.doctest',
-    'sphinx.ext.extlinks',
-    # See: https://www.sphinx-doc.org/en/master/usage/extensions/githubpages.html
-    # See: https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
-    'sphinx.ext.githubpages',
-    'sphinx.ext.intersphinx',
-    # 'sphinx.ext.linkcode',
-    # 'recommonmark',
-    'sphinxcontrib.programoutput',
-]
-extlinks = {
-    'github-branch': ('https://github.com/insurgency/aioa2squery/tree/branches/%s', 'name'),
-    'issue': ('https://github.com/insurgency/aioa2squery/issues/%s', 'gh-'),
-    'valve-wiki': ('https://developer.valvesoftware.com/wiki/%s', 'page'),
-    'wikipedia': ('https://wikipedia.org/wiki/%s', 'page'),
-    'steam-app': ('https://store.steampowered.com/app/%s', 'app-id'),
-    'steam-support': ('https://support.steampowered.com/kb_article.php?ref=%s', 'ref'),
-}
-# https://github.com/NextThought/sphinxcontrib-programoutput/issues/37#issuecomment-504008704
-programoutput_prompt_template = "$ a2squery query --help\n{output}"
-intersphinx_mapping = {
-    'py': ('https://docs.python.org/{0.major}.{0.minor}'.format(sys.version_info), None),
-}
 cwd = '../../'
-autodoc_member_order = 'bysource'
-templates_path = ['_templates']
 exclude_patterns = []
-html_theme = 'alabaster'
-html_static_path = ['_static']
-nitpicky = True
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
+
+# Extension Settings
+
+# See: https://github.com/NextThought/sphinxcontrib-programoutput/issues/37#issuecomment-504008704
+programoutput_prompt_template = "$ a2squery query --help\n{output}"
 
 # Spelling: https://sphinxcontrib-spelling.readthedocs.io/en/stable/install.html#configuration
-
 # Spell checking needs an additional module. Add it only if spell checking is requested so docs can be generated without
 # it.
 if 'spelling' in sys.argv:
